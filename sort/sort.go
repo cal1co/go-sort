8ebd507ae79efc06eb346b2008e52c1a0fbb65d1
Alex King <calicoalix@gmail.com>
2023-10-11 23:09:30 +1100

    updated description for sort

--- sort/sort.go
+++ sort/sort.go
@@ -2,6 +2,11 @@ package sort
 
 import "math/rand"
 
+// Shuffle is a linear time shuffle operation that reliably reorganizes an integer array
+//
+// Time Complexity: O(N)
+//
+// Space: O(1)
 func Shuffle(arr []int) {
 	for i := 0; i < len(arr); i++ {
 		idx := rand.Intn(len(arr))
